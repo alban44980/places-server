dd6d57c1509aaac0739a5a03829621f6090a102d
AdrianoG117 <adrianogonzalezalberto@gmail.com>
2021-10-11 10:12:03 +0200

    added connection config to app.ts, created tsconfig

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
 import express from 'express';
+import config from "config";
+
+
+
+const port = config.get<number>('PORT');
+const app = express();
+
+
+app.listen(port, ()=>{
+  console.log("server is running");
+});
