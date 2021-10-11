74211a1acd8363eff92dc139f0f633eccbcba4fb
AdrianoG117 <adrianogonzalezalberto@gmail.com>
2021-10-11 11:31:44 +0200

    added routes shell

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import config from "config";
 import logger from "./utils/logger";
-
+import routes from './routes';
 
 
 const port = config.get<number>('port');
@@ -10,4 +10,6 @@ const app = express();
 
 app.listen(port, ()=>{
   logger.info(`server is running at http://localhost:${port}`);
+  routes(app);
+  
 });
